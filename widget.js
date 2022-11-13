43a12a742e867d52a5c5f985d8c5da199a5afff3
Ahmed Mokhtar <85036236+AhmedMokhtar5483@users.noreply.github.com>
2022-11-14 00:04:54 +0200

    card_design_1

---
+++ widget.js
@@ -0,0 +1,2 @@
+<div class="College_Projects" data-github="AhmedMokhtar5483/College_Projects" data-width="400" data-height="" data-theme="default"></div>
+<script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
